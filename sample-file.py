d32f8e7f3126f8243eb38e42eb0e126de36fe7d3
Mark Nicholas Razon <84964385+marknicholasrazon@users.noreply.github.com>
2023-09-20 18:40:30 +0800

    Add files via upload

---
+++ sample-file.py
@@ -0,0 +1,45 @@
+from mysql.connector import Error
+import mysql.connector
+
+# Establish a connection
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='sampledb'
+    )
+
+    if connection.is_connected():
+        #print(f'Connected to MySQL database: {connection.database}')
+
+        # Perform database operations here
+
+        try:
+            cursor = connection.cursor()
+
+            # Example query
+            cursor.execute("SELECT * FROM product")
+
+            # Fetch and print results
+            records = cursor.fetchall()
+            for record in records:
+                print(record) 
+                
+
+        except Error as e:
+            print(f"Error: {e}")
+
+        finally:
+            # Close the cursor
+            if 'cursor' in locals():
+                cursor.close()
+
+except Error as e:
+    print(f"Error: {e}")
+
+finally:
+    # Close the connection
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        #print('Connection closed.')
